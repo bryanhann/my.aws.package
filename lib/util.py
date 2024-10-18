1cf4b7a4426097afd420bcd6447170f8bbbbe248
Bryan Hann <nobody@nowhere>
2024-10-18 20:00:31 -0230

    Add utility functions.

--- lib/util.py
+++ lib/util.py
@@ -7,6 +7,10 @@ def stderr(txt):
     sys.stderr.write(str(txt) + '\n')
     sys.stderr.flush()
 
+def stdout(txt):
+    sys.stdout.write(str(txt) + '\n')
+    sys.stdout.flush()
+
 def bold(text):
     return Style.BRIGHT + Fore.RED + text + Style.RESET_ALL
 
@@ -18,4 +22,13 @@ def run(line):
     stderr( '\nRunning:')
     stderr(f'    {bold(line)}')
     subprocess.run( line.split() )
+
+def ezrun(line, capture=True, show=False):
+    if show:
+        stderr(bold(line))
+    it=subprocess.run(line.split(), capture_output=capture)
+    if capture:
+        it.stdout = it.stdout.decode('utf-8')
+        it.stderr = it.stderr.decode('utf-8')
+    return it
 
